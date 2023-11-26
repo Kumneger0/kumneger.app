943d445097e0176b574d74a2e1b0eaffd2de1dea
kumneger Wondimu <93655689+Kumneger0@users.noreply.github.com>
2023-11-26 22:25:55 +0300

    Update wrapper.tsx

--- src/app/(blog)/blog/[slug]/wrapper.tsx
+++ src/app/(blog)/blog/[slug]/wrapper.tsx
@@ -3,14 +3,14 @@ import Blog from "./blog";
 import React from "react";
 import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { SessionProvider } from "next-auth/react";
-import Comments from "@/components/comments";
+// import Comments from "@/components/comments";
 
 
 function Wrapper({ children }: { children: MDXRemoteSerializeResult }) {
   return <>
     <SessionProvider>
-      <Blog blogContent={children} />;
-      <Comments />
+      <Blog blogContent={children} />
+
     </SessionProvider>
 
   </>
